5bfd1493fc84dd551d2101c875655db90c3da188
tobiolabode <tobi@tobiolabode.com>
2018-09-04 20:44:37 +0100

    added more dependencees

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import argparse
+import sys
+import time
+import os
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 
 layers = tf.keras.layers
 
